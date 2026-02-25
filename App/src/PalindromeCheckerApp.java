e1a5aaac6485bc633f458ca5def39f893292ba36
Abhay <abhaymittal3333@gmail.com>
2026-02-25 09:21:08 +0530

    Abhay Re-added UC1 Welcome Page Clean

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,7 +1,12 @@
+// version 1.0
+// author Abhay
+// useCase 1: Welcome page
+
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to Palindrome Checker App Management System");
+
     }
 }
