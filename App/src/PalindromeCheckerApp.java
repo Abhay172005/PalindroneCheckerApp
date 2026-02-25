babf12a41af55e9708f9908a87fcb39594c0b770
NIKHIL-KUMAR1217 <ajay.2005nikhil@gmail.com>
2026-02-25 09:08:34 +0530

    Abhay Added UC2 Hardcoded Palindrome Logic

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -3,5 +3,23 @@ public class PalindromeCheckerApp {
     public static void main(String[] args) {
 
         System.out.println("Welcome to Palindrome Checker App Management System");
+
+        String word = "madam";
+
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < word.length() / 2; i++) {
+
+            if (word.charAt(i) != word.charAt(word.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println(word + " is a Palindrome.");
+        } else {
+            System.out.println(word + " is NOT a Palindrome.");
+        }
     }
 }
