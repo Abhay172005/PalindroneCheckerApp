790ff78c46c14637bd17f9527284d7449578e62c
Abhay172005 <abhaymittal3333@gmail.com>
2026-02-25 10:10:38 +0530

    Abhay Added UC1 Welcome Page via PR

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,91 +1,12 @@
-// version 8.0
+// version 1.0
 // author Abhay
-// useCase 8: Linked List Based Palindrome Checker
+// useCase 1: Welcome Page
 
 public class PalindromeCheckerApp {
-
-    // Node class for singly linked list
-    static class Node {
-        char data;
-        Node next;
-
-        Node(char data) {
-            this.data = data;
-            this.next = null;
-        }
-    }
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to Palindrome Checker App Management System");
 
-        String word = "madam";
-
-        // Convert string to linked list
-        Node head = null;
-        Node tail = null;
-
-        for (int i = 0; i < word.length(); i++) {
-            Node newNode = new Node(word.charAt(i));
-
-            if (head == null) {
-                head = newNode;
-                tail = newNode;
-            } else {
-                tail.next = newNode;
-                tail = newNode;
-            }
-        }
-
-        // Check palindrome using linked list logic
-        boolean isPalindrome = isPalindrome(head);
-
-        if (isPalindrome) {
-            System.out.println(word + " is a Palindrome.");
-        } else {
-            System.out.println(word + " is NOT a Palindrome.");
-        }
-    }
-
-    // Function to check palindrome in linked list
-    public static boolean isPalindrome(Node head) {
-
-        if (head == null || head.next == null) {
-            return true;
-        }
-
-        // Step 1: Find middle using fast & slow pointer
-        Node slow = head;
-        Node fast = head;
-
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        // Step 2: Reverse second half
-        Node prev = null;
-        Node current = slow;
-
-        while (current != null) {
-            Node nextNode = current.next;
-            current.next = prev;
-            prev = current;
-            current = nextNode;
-        }
-
-        // Step 3: Compare first half and reversed second half
-        Node firstHalf = head;
-        Node secondHalf = prev;
-
-        while (secondHalf != null) {
-            if (firstHalf.data != secondHalf.data) {
-                return false;
-            }
-            firstHalf = firstHalf.next;
-            secondHalf = secondHalf.next;
-        }
-
-        return true;
     }
 }
